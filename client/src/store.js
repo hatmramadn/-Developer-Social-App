8ccf6b367c7993315d4c400bf625df52080eadfb
hatem <hatem.timooo@gamil.com>
2019-05-07 02:02:52 +0200

    removed dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,10 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
-
 export default store;
